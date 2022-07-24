6434d63ebadff7539bc2729ab7686598a9d779db
bou2201 <lamchuc123456@gmail.com>
2022-07-24 17:19:47 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -86,16 +86,6 @@
     },
     true
   );
-
-  // Animation on scroll
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
 })();
 
 // Animation text hero
